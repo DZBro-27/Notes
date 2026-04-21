1b00cba80a44cd4d1f942dd1d08ef25929193b8f
DZBro-27 <grimmer.27reg@gmail.com>
2026-04-21 22:48:32 +0300

    feat: add build_note function

--- notes_app.py
+++ notes_app.py
@@ -1,3 +1,11 @@
 import os
 import re
 
+
+def build_note(note_text, note_name):
+    try:
+        with open(f"{note_name}.txt", "w", encoding="utf-8") as file:
+            file.write(note_text)
+        print(f"Заметка {note_name} создана!")
+    except OSError as e:
+        print(f"Ошибка при работе с файлом: {e}")
