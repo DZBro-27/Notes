e4e0bfde28323d8c822700b1dd2a2ab7a0278981
DZBro-27 <grimmer.27reg@gmail.com>
2026-04-22 00:41:22 +0300

    feat: add create_note function

--- notes_app.py
+++ notes_app.py
@@ -9,3 +9,25 @@ def build_note(note_text, note_name):
         print(f"Заметка {note_name} создана!")
     except OSError as e:
         print(f"Ошибка при работе с файлом: {e}")
+
+
+def create_note():
+    """Создает файл с заметкой"""
+
+    forbidden_symbols = "\\|/*<>?:"
+
+    while True:
+        note_name = input("Введите название заметки: ").strip()
+
+        if not note_name:
+            print("Название не может быть пустым.")
+
+        elif any(symbol in note_name for symbol in forbidden_symbols):
+            print("Недопустимые символы в названии. Попробуйте снова!")
+
+        else:
+            break
+
+    note_text = input("Введите текст заметки: ")
+
+    build_note(note_text, note_name)
